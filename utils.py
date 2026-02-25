bad70bc0d2a59cfbdab84e1c21b7b2b760f46412
sarthak213 <maverick555.sp2003@gmail.com>
2026-02-25 18:51:53 +0530

    configuration file added. Logic separated from config. Added a Reverse Mode and Debug toggle in config file

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
-def generate_reply(message):
+from config import MODEL_NAME, MAX_HISTORY, DEBUG_MODE, REVERSE_MODE
 
-    if "Python" in message:
-        return "So you like Python Huh? 😎"
-    elif "Java" in message:
-        return "Java is a great language too!"
-    elif message.islower():
-        return "I heard you say " + message.upper() + " in lowercase!"
-    elif message.isupper():
-        return "I heard you say " + message.lower() + " in uppercase!"
-    else:
-        return "I heard you say " + message
+def generate_reply(message: str) -> str:
+    if DEBUG_MODE:
+        print(f"[DEBUG] Generating reply for message: {message}")
+        if "Python" in message:
+            return "So you like Python Huh? 😎"
+        elif "Java" in message:
+            return "Java is a great language too!"
+        elif message.islower():
+            return "I heard you say " + message.upper() + " in lowercase!"
+        elif message.isupper():
+            return "I heard you say " + message.lower() + " in uppercase!"
+        elif REVERSE_MODE:
+            return "I heard you say " + message[::-1] + " in reverse!"
+
+    return "I heard you say " + message
